13553c5049337b33142ec3d7aa13c757cb7969b7
Raghavendran Suryanarayanan <raghav2787@gmail.com>
2016-07-12 23:58:31 +0100

    second google ad on top

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,70 +18,80 @@ import {Observable} from 'rxjs/Observable'
     providers: [ProductService, HTTP_PROVIDERS],//define it only once here so it will be a singleton instance
     template: `
 	<div id="sticky-anchor"></div>
-   <div id="sticky" class="menuSection">
-   <div class="bg"></div>
-            <span class="showMenuOnMobile">Show on Mobile</span>
-            <div>
-                <a href="#">
-                    <div class="logoSection">
-                       
-                        
-                    </div>
-                </a>
-                <div id="footnote"> 
-                    <div class="clearfix"> 
-                    <div class="connect"> 
-                    <a href="http://www.facebook.com/sharer.php?u=http://dealsofworld.com/" target="_blank" title="Share on Facebook" class="facebook"></a>
-                    <a href="http://twitter.com/home?status=Currently reading http://dealsofworld.com/" target="_blank" title="Share on Twitter" class="twitter"></a>
-                    <a href="https://plus.google.com/share?url=http://dealsofworld.com/" target="_blank" title="Share on Google+" class="googleplus"></a> 
-                    </div> 
-                    </div> 
-                   <div class="showMoreButton red"><div class="shine"></div><a href="#">Sign in</a></div>
-                 </div>
-				<div style="padding-top:2%">
-        <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><img src="app/Assets/Images/{{country}}.png" />
-  <span class="caret"></span></button>
-  <ul class="dropdown-menu">
-    <li><a href="#" (click)="ChangeCountry('uk')"><img src="app/Assets/Images/UK.png" alt="" /><span> UK</span></a></li>
-    <li><a href="#" (click)="ChangeCountry('us')"><img src="app/Assets/Images/US.png" alt="" /><span>  USA</span></a></li>
-    <li><a href="#" (click)="ChangeCountry('in')"><img src="app/Assets/Images/IN.png" alt="" /><span>  India</span></a></li>
-  </ul>
-</div>	          
-<section class="webdesigntuts-workshop">
+<div id="sticky" class="menuSection">
 
-	<form>	
+  <span class="showMenuOnMobile">Show on Mobile</span>
+  <div>
+    <a href="#">
+      <div class="logoSection">
+
+
+      </div>
+    </a>
     
-		<input [(ngModel)]="searchString" type="search"  placeholder="What are you looking for?">		    	
-		<button (click)="SearchProducts()">Search</button>
-	</form>
-     
-</section>
-          
-                <div class="menuLists">
-                    <ul>
-                        <li *ngFor='#menu of menus'><button class="button button1" (click)="LoadProductsAndMenus(menu, true)"><span>{{menu}}</span></button></li>
-                    </ul>
-                </div> 
-				</div>
-            </div>
+    <div id="footnote">
+      <div class="clearfix">
+        <div class="connect">
+          <a href="http://www.facebook.com/sharer.php?u=http://dealsofworld.com/" target="_blank" title="Share on Facebook" class="facebook"></a>
+          <a href="http://twitter.com/home?status=Currently reading http://dealsofworld.com/" target="_blank" title="Share on Twitter"
+            class="twitter"></a>
+          <a href="https://plus.google.com/share?url=http://dealsofworld.com/" target="_blank" title="Share on Google+" class="googleplus"></a>
         </div>
-		<div class="bodyWrapper">
-        <div id="sideMenu" >
-            <ul id="menu-v">
-                <li *ngFor='#menu of sideMenus'><a href="#" (click)="LoadProductsAndMenus(menu, false)"><span class="glyphicon glyphicon-tag"></span> {{menu}} </a></li>
-                </ul>
-			  
-        </div>
-		<div style="float:left;width:73%;"> 
-			  <dw-products [selectedMenu]= "userselectedMenu"></dw-products>
-			  </div>
+      </div>
+      <div class="showMoreButton red">
+        <div class="shine"></div><a href="#">Sign in</a></div>
+    </div>
+    <div style="padding-top:2%">
+      <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><img src="app/Assets/Images/{{country}}.png" />
+  <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+          <li>
+            <a href="#" (click)="ChangeCountry('uk')"><img src="app/Assets/Images/UK.png" alt="" /><span> UK</span></a>
+          </li>
+          <li>
+            <a href="#" (click)="ChangeCountry('us')"><img src="app/Assets/Images/US.png" alt="" /><span>  USA</span></a>
+          </li>
+          <li>
+            <a href="#" (click)="ChangeCountry('in')"><img src="app/Assets/Images/IN.png" alt="" /><span>  India</span></a>
+          </li>
+        </ul>
+      </div>
+      <section class="webdesigntuts-workshop">
 
-			  
-			  </div>
-  
+        <form>
+
+          <input [(ngModel)]="searchString" type="search" placeholder="What are you looking for?">
+          <button (click)="SearchProducts()">Search</button>
+        </form>
+
+      </section>
+
+      <div class="menuLists">
+        <ul>
+          <li *ngFor='#menu of menus'><button class="button button1" (click)="LoadProductsAndMenus(menu, true)"><span>{{menu}}</span></button></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bodyWrapper">
+  <div id="sideMenu">
+    <ul id="menu-v">
+      <li *ngFor='#menu of sideMenus'><a href="#" (click)="LoadProductsAndMenus(menu, false)"><span class="glyphicon glyphicon-tag"></span> {{menu}} </a></li>
+    </ul>
+
+  </div>
+  <div style="float:left;width:73%;">
+    <dw-products [selectedMenu]="userselectedMenu">
+      <div class="modal"></div>
+    </dw-products>
+  </div>
+
+
+</div>
+
 <div>
-
     `
     
 })
